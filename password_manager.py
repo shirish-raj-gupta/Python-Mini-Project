990de84f93a6fe7448fcfb204289774a975f129a
shirish-raj-gupta <codersrg@gmail.com>
2025-06-11 10:20:53 +0530

    set up of env

--- password_manager.py
+++ password_manager.py
@@ -1,17 +1,37 @@
-master_pwd = input('What is your master password? ')
+from cryptography.fernet import Fernet
 
+def load_key():
+  file = open('key.key', 'rb')
+  key = file.read()
+  file.close()
+  return key
+
+
+master_pwd = input('What is your master password? ')
+key = load_key() + master_pwd.encode()
+fer = Fernet(key)
+
+
+'''def write_key():
+  key = Fernet.generate_key()
+  with open('key.key','wb')as key_file:
+    key_file.write(key)'''
+    
+    
+
+  
 def view():
   with open('passwords.txt', 'r') as f:
     for line in f.readline():
       data = line.rstrip
       user,passw = data.split('|')
-      print('User: ' , user , 'Password: ', passw)
+      print('User: ' , user , 'Password: ', str(fer.decrypt(passw.encode())))
 
 def add():
   name = input('Account name: ')
   pwd = input('Password: ')
   with open('passwords.txt', 'a') as f:
-    f.write(name+'|'+ pwd +'\n')
+    f.write(name+'|'+ str(fer.encrypt(pwd.encode())) +'\n')
     
 
 
